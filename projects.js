73eb127f267850f2d7e61171051825e22fcac3c1
Eduardo Debarba Scheuermann <eduardodsdev@gmail.com>
2023-03-30 16:33:21 -0300

    tests

---
+++ projects.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('#show-or-hide')
+var sec = document.querySelector('.containerr')
+
+btn.addEventListener('click', function(){
+    if(sec.style.display === 'block'){
+        sec.style.display === 'none'
+    } else{
+        sec.style.display === 'block'
+    }
+})
