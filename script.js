b7197066706c2a9c8f38a913bf84191592e09dbd
EduardoDebarba <eduardodsdev@gmail.com>
2025-04-15 15:42:53 -0300

    created and styled section on services, experiences, partner companies, customer reviews and repair gallery

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
-let show = true;
-
-const menuSection = document.querySelector(".menu-section");
-const menuToggle = menuSection.querySelector(".menu-toggle");
-
-menuToggle.addEventListener("click", () => {
-  document.body.style.overflow = show ? "hidden" : "initial";
-  menuSection.classList.toggle("on", show);
-  show = !show;
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-
-
-let cont = document.querySelector('.more-projects')
-
-let btnClick = function(){
-  if(cont.style.display === 'flex'){
-    cont.style.display = 'none'
-  }else{
-    cont.style.display = 'flex'
-  }
-}
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function () {
-  nextImage();
-}, 5000);
-
-function nextImage() {
-  count++;
-  if (count > 3) {
-    count = 1;
-  }
-
-  document.getElementById("radio" + count).checked = true;
-}
-
-window.sr = ScrollReveal({reset: true})
-
-sr.reveal('.scroll', {duration: 2000})
